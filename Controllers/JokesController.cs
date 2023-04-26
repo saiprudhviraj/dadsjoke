df85f0971d6c49249430e9a50a0ad5dda21a41c3
sraj_arborgold <sraj@arborgold.com>
2023-04-26 13:12:57 +0530

    reinitialized files

---
+++ Controllers/JokesController.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using dadsjoke.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DadJokesApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class JokeController : ControllerBase
+    {
+        private readonly JokeService _jokeService;
+
+        public JokeController(JokeService jokeService)
+        {
+            _jokeService = jokeService;
+        }
+
+        [HttpGet("random")]
+        public async Task<ActionResult<string>> GetRandomJoke()
+        {
+            var joke = await _jokeService.GetRandomJoke();
+            return Ok(joke);
+        }
+
+        [HttpGet("count")]
+        public async Task<ActionResult<int>> GetJokeCount()
+        {
+            var count = await _jokeService.GetJokeCount();
+            return Ok(count);
+        }
+    }
+}
---
+++ Services/JokeService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace dadsjoke.Services
+{
+    public class JokeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public JokeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<string> GetRandomJoke()
+        {
+            var response = await _httpClient.GetAsync("https://icanhazdadjoke.com/");
+            response.EnsureSuccessStatusCode();
+
+            var json = await response.Content.ReadAsStringAsync();
+            var data = JsonConvert.DeserializeObject<JokeData>(json);
+
+            return data.joke;
+        }
+
+        public async Task<int> GetJokeCount()
+        {
+            var response = await _httpClient.GetAsync("https://icanhazdadjoke.com/");
+            response.EnsureSuccessStatusCode();
+
+            var headers = response.Headers;
+            if (headers.Contains("X-Total-Count"))
+            {
+                return int.Parse(headers.GetValues("X-Total-Count").FirstOrDefault());
+            }
+            else
+            {
+                throw new Exception("X-Total-Count header not found.");
+            }
+        }
+    }
+
+    public class JokeData
+    {
+        public string id { get; set; }
+        public string joke { get; set; }
+        public List<string> categories { get; set; }
+    }
+}
